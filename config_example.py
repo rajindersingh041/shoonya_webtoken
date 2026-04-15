a653c9ff0651349618d5d5022930c29e65ab12b9
Rajindersingh041 <rajindersingh041@gmail.com>
2026-04-15 16:31:15 +0200

    init updated

---
+++ config_example.py
@@ -0,0 +1,10 @@
+user_configs = {
+    "C210": {
+        "user_id": "",
+        "pwd": "",
+        "api_key": "",
+        "totp": "",
+        "secret": "",
+    },
+    "XXXXX3": {"user_id": "", "pwd": "", "api_key": "", "totp": "", "secret": ""},
+}
